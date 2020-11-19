74c41b9eea00a86d3ad682e6521403d9e41778d6
Kageschwump <kasperfalch@live.dk>
2020-11-19 14:57:50 +0100

    opdateret account klassen

--- src/main/java/Model/Account.java
+++ src/main/java/Model/Account.java
@@ -1,4 +1,23 @@
 package Model;
 
 public class Account {
+    private int balance;
+    private Player player;
+
+    public Account(int balance, Player player){
+        this.balance = balance;
+        this.player = player;
+    }
+
+    public void updateScore(int balanceUpdate){
+        if(balance+balanceUpdate < 0)
+        {
+           balance = 0;
+        } else {
+            balance += balanceUpdate;
+        }
+    }
+
+    public int getBalance(){return balance;}
+
 }
