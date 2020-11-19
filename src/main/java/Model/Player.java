20ac3581ada7a591eb610ac13041502c4c4958ca
Kageschwump <kasperfalch@live.dk>
2020-11-19 14:13:37 +0100

    lavet player-klassen

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,8 +1,31 @@
 package Model;
 
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
+
 public class Player {
     private String name;
     private int id;
-    private
+    private GUI_Car car;
+    private GUI_Player player;
+    private Account account;
+    private int placement;
 
+    public Player(int id, String name, GUI_Player player, GUI_Car car, Account account){
+        this.name = name;
+        this.id = id;
+        this.player = player;
+        this.car = car;
+        this.account = account;
+    }
+
+    public String getName(){return name;}
+
+    public int getId(){return id;}
+
+    public GUI_Player getPlayer(){return player;}
+
+    public void updatePlacement(int newPlacement){
+
+    }
 }
