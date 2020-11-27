caf000641082e76b768b0204a699aea925af8450
Kageschwump <kasperfalch@live.dk>
2020-11-27 17:02:35 +0100

    ændret på funktionen i prisonsquare

--- src/main/java/Model/SquareTypes/PrisonSquare.java
+++ src/main/java/Model/SquareTypes/PrisonSquare.java
@@ -10,7 +10,7 @@ import java.awt.*;
 public class PrisonSquare extends GameSquare {
 
     private String name = "Fængsel";
-    private String description = "Surt show, du røg i fængsel";
+    private String description = "";
     private Color bgColor = Color.white;
     private Color fgColor = Color.black;
     private GUI_Field fieldType;
@@ -27,6 +27,13 @@ public class PrisonSquare extends GameSquare {
         {
             player.setPlacement(6);
             player.getAccount().updateScore(-1);
+            this.description = "Surt show, du røg i fængsel. du betaler 1 penge for at blive løsladt";
+        }
+
+        else{
+            player.setPlacement(6);
+            this.description = "Du røg i fængsel, men du bliver løsladt gratis da du har et chancekort";
+            player.setJailFreeCard(false);
         }
     }
 
