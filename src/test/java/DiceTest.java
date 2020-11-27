e9900262b3d0ba333fe4367c118056a172a295ee
harzzek <39692510+harzzek@users.noreply.github.com>
2020-11-27 17:09:55 +0100

    Opstillet spillets til ende

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,22 @@
+import Model.Dice;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DiceTest {
+
+    @Test
+    public void CheckIfDiceRollsAboveSix()
+    {
+        boolean aboveSixAndBelowOne = false;
+        Dice dice = new Dice();
+
+        for (int i = 0; i < 10000; i++) {
+            dice.roll();
+
+            if (dice.getFaceValue() > 6 || dice.getFaceValue()<1) {
+                aboveSixAndBelowOne = true;
+            }
+        }
+        Assert.assertFalse(aboveSixAndBelowOne);
+    }
+}
