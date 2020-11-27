18f29c129bb36a3233293a324cfef779cea52f7d
Kageschwump <kasperfalch@live.dk>
2020-11-27 18:28:19 +0100

    lavet en checkifgameends test

--- src/test/java/GameOverTest.java
+++ src/test/java/GameOverTest.java
@@ -1,3 +1,4 @@
+import Controller.GameHandler;
 import Model.Player.Player;
 import Model.RuleSet;
 import org.junit.Assert;
@@ -20,5 +21,17 @@ public class GameOverTest {
         Assert.assertEquals(ruleSet.determineWinner(players),players[1]);
 
         //
+    }
+
+    @Test
+    public void ChecksIfGameEnds(){
+        RuleSet ruleset = new RuleSet(null, null);
+        Player[] players = new Player[4];
+        players[0] = new Player(0,"HC",22,null,5);
+        players[1] = new Player(1,"Kasper",22,null,10);
+        players[2] = new Player(2,"Henrik",21,null,-4);
+        players[3] = new Player(2,"Filip",23,null,0);
+
+        Assert.assertTrue(ruleset.gameOver(players));
     }
 }
